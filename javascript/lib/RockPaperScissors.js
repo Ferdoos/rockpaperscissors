3dcd8d236bd0e789bf6d17c1ebfb901cfbcfe8bf
Ferdoos Amin <Ferdoos@gmail.com>
2013-11-11 16:54:58 +0000

    second commit

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,5 +1,10 @@
-function Player () {
+function Player(name) {
+		this.name = name;
+};
 
+Player.prototype.picks = function(pick) {
+	this.pick = pick;
+	console.log(this.name+" picked "+pick);
 };
 
 function Game (player1, player2) {
@@ -8,22 +13,19 @@ function Game (player1, player2) {
 	this.player2 = player2;
 };
 
-Player.prototype.picks = function(pick) {
-	this.pick = pick;
-};
-
 Game.prototype.winner = function() {
+	//	return player2
 
 	this.pairs = {
 		'rock': { 'beats': 'scissors' },
 		'paper': { 'beats': 'rock'},
 		'scissors': { 'beats': 'paper' } 
-		};
+};
 
 	if (this.player1.pick == this.player2.pick) {
 		return null;
 	}
-	else if (this.pairs[this.player1.pick]['beats'] == this.player2.pick) {
+ 	if (this.pairs[this.player1.pick]['beats'] == this.player2.pick) {
 		return this.player1;
 	}
 	else {	
